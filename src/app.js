9581d85376c5ddc8221c315570f5421d286cf601
Bartosz Jurczewski <Bartosz.Jurczewski@rossmann.com.pl>
2020-01-29 14:32:03 +0100

    'Reading Input Data' done

--- src/app.js
+++ src/app.js
@@ -8,18 +8,41 @@ const budgetController = (() => {
 // UI CONTROLLER
 const UIController = (() => {
 
-    // Some code
+    const DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,// Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }   
+        },
+
+        getDOMstrings: () => {
+            return DOMstrings;
+        }
+    };
 
 })();
-
 
 
 // GLOBAL APP CONTROLER
 const controller = ((budgetCtrl, UICtrl) => {
 
+    const DOM = UICtrl.getDOMstrings();
+
     const ctrlAddItem = () => {
 
         //  1. Get the filed input data
+
+        const input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -29,16 +52,17 @@ const controller = ((budgetCtrl, UICtrl) => {
 
         // 5. Display the budget on the UI
 
-        console.log('It works.');
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', (event) => {
 
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();        
         }
+
+        
 
     });
 
