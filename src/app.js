9a883f287857060a2dcc9b8b275b92f0567b0d70
Bartosz Jurczewski <Bartosz.Jurczewski@rossmann.com.pl>
2020-01-29 16:30:39 +0100

    'Creating Income and Expense Function Constructors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 // BUDGET CONTROLLER
 const budgetController = (() => {
 
-    // Some code
+    const Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 })();
+
+const Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
 
 // UI CONTROLLER
 const UIController = (() => {
