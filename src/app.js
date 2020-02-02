84561c60682910fbedcff6a7290e35111eadbd35
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2020-02-02 12:07:00 +0100

    Budget Controler (calc) and UI updates

--- src/app.js
+++ src/app.js
@@ -21,17 +21,25 @@ const budgetController = (() => {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percantage: -1
+    };
+
+    const calculateTotal = (type) => {
+        let sum = 0;
+        data.allItems[type].forEach(cur => sum += cur.value);
+        data.totals[type] = sum;
     };
 
     return {
         addItem: (type, des, val) => {
             let newItem, ID;
-            
+
             // Create new ID
-            if(data.allItems[type].length > 0){
+            if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-            }else{
+            } else {
                 ID = 0;
             }
 
@@ -48,6 +56,32 @@ const budgetController = (() => {
 
             // Return the new element
             return newItem;
+        },
+
+        calculateBudget: () => {
+
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the bugdet: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percantage of income we spent
+            if (data.totals.inc > 0) {
+                data.percantage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percantage = -1;
+            }
+        },
+
+        getBudget: () => {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percantage: data.percantage
+            }
         },
 
         testing: () => {
@@ -67,6 +101,10 @@ const UIController = (() => {
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
         expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
 
     return {
@@ -83,7 +121,7 @@ const UIController = (() => {
             // Replace the placeholder text with some actual data
             let html, element;
 
-            if( type === 'inc'){
+            if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
                 html = `
                 <div class="item clearfix" id="income-${obj.id}">
@@ -95,7 +133,7 @@ const UIController = (() => {
                         </div>
                     </div>
                 </div>`;
-            } else if( type ==='exp'){
+            } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
 
                 html = `                        
@@ -108,7 +146,7 @@ const UIController = (() => {
                         </div>
                     </div>
                 </div>`;
-            }  
+            }
 
             // Insert HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeEnd', html);
@@ -125,6 +163,19 @@ const UIController = (() => {
             });
 
             fieldsArray[0].focus();
+        },
+
+        displayBudget: (obj) => {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percantage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percantage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
         },
 
         getDOMstrings: () => {
@@ -154,18 +205,21 @@ const controller = ((budgetCtrl, UICtrl) => {
     const updateBudget = () => {
 
         // 1. Calculate budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        const budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-    }
+        UICtrl.displayBudget(budget);
+    };
 
     const ctrlAddItem = () => {
 
         //  1. Get the filed input data
         const input = UICtrl.getInput();
 
-        if(input.description != "" && !isNaN(input.value) && input.value > 0){
+        if (input.description != "" && !isNaN(input.value) && input.value > 0) {
             // 2. Add the item to the budget controller
             const newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -183,6 +237,12 @@ const controller = ((budgetCtrl, UICtrl) => {
     return {
         init: () => {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percantage: -1
+            });
             setupEventListeners();
         }
     }
